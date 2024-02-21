7d0be6194f979220a14a6e3eb80d49ca5eef916e
niisanS14 <159295866+niisanS14@users.noreply.github.com>
2024-02-21 17:23:29 -0600

    Add files via upload

---
+++ cs310-junit-hello-world-master/src/edu/jsu/mcis/cs310/Main.java
@@ -0,0 +1,27 @@
+package edu.jsu.mcis.cs310;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Main m = new Main();
+        String message = m.getGreeting();
+        
+        System.out.println(message);
+        System.out.println(m.reverse(message));
+        
+    }
+    
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+    
+    public String reverse(String message) {
+        
+        StringBuilder str = new StringBuilder(message);
+        str.reverse();
+        return str.toString();
+        
+    }
+    
+}
---
+++ cs310-junit-hello-world-master/test/edu/jsu/mcis/cs310/ExampleTest.java
@@ -0,0 +1,57 @@
+package edu.jsu.mcis.cs310;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    private Main main;
+    private String expectedGreeting, expectedReverseGreeting;
+    private String expectedReverse1, expectedReverse2, expectedReverse3, expectedReverse4;
+    
+    @Before
+    public void setUp() {
+        main = new Main();
+        expectedGreeting = "Hello, World!";
+        expectedReverseGreeting = "!dlroW ,olleH";
+        expectedReverse1 = "topaeT elttiL a m'I";
+        expectedReverse2 = "elddiD elddiD yeH";
+        expectedReverse3 = "kcoD yrokciD yrokciH";
+        expectedReverse4 = "hay ot gninroM";
+    }
+        
+    @Test
+    public void testGreeting() {
+        String actual = main.getGreeting();
+        assertEquals(expectedGreeting, actual);
+    }
+    
+    @Test
+    public void testReverseGreeting() {
+        String actual = main.reverse(expectedGreeting);
+        assertEquals(expectedReverseGreeting, actual);
+    }
+    
+    @Test
+    public void testReverseMessage1() {
+        String actual = main.reverse("I'm a Little Teapot");
+        assertEquals(expectedReverse1, actual);
+    }
+    
+    @Test
+    public void testReverseMessage2() {
+        String actual = main.reverse("Hey Diddle Diddle");
+        assertEquals(expectedReverse2, actual);
+    }
+    
+    @Test
+    public void testReverseMessage3() {
+        String actual = main.reverse("Hickory Dickory Dock");
+        assertEquals(expectedReverse3, actual);
+    }
+    @Test
+    public void testReverseMessage4() {
+        String actual = main.reverse("Morning to yah");
+        assertEquals(expectedReverse4, actual);
+    }
+}
