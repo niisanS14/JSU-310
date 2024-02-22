7267526a3d43bc95fd6d64dee9bda1c13d9ae22c
niisanS14 <159295866+niisanS14@users.noreply.github.com>
2024-02-21 18:02:13 -0600

    Add files via upload

---
+++ cs310-hello-world-sp24-master/Class1.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Daniel Holcombe
+ */
+
+public class Class1 {
+
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        //  (v2.0)
+        }
+        private void f3() {
+            // hotfix (v3.0)
+            }
+            private void f4() {
+                // complete (v4.0)
+                }
+    
+}
